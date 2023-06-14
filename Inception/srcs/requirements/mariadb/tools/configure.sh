ba92db76241feb54b25eeed804677b85f8077a23
Rolf Haring <rharing@f1r6s4.codam.nl>
2023-06-14 15:43:33 +0200

    af

--- Inception/srcs/requirements/mariadb/tools/configure.sh
+++ Inception/srcs/requirements/mariadb/tools/configure.sh
@@ -1,9 +1,19 @@
 #!bin/sh
 
-echo "CREATE USER IF NOT EXITS $MYSQL_USERNAME;"
-echo "CREATE USER IF NOT EXISTS $MYSQL_USERNAME@'%' IDENTIFIED BY '$MYSQL_PASSWORD';"
-echo "GRANT ALL ON *.* TO $MYSQL_USERNAME@'%' IDENTIFIED BY '$MYSQL_PASSWORD';"
-echo "FLUSH PRIVILEGES;"
-mysqld --bootstrap
+if [ ! -d "/var/lib/mysql/$MYSQL_DATABASE" ]; then
+    echo "Setting up Mariadb"
+
+    mysql_install_db --user=mysql --datadir=/var/lib/mysql
+    sed -i "s/bind-address.*/bind-address = $MYSQL_HOSTNAME/" /etc/mysql/mariadb.conf.d/50-server.cnf
+    {
+        echo "FLUSH PRIVILEGES;"
+        echo "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;"
+        echo "CREATE USER IF NOT EXISTS $MYSQL_USERNAME@'%' IDENTIFIED BY '$MYSQL_PASSWORD';"
+        echo "GRANT ALL ON *.* TO $MYSQL_USERNAME@'%' IDENTIFIED BY '$MYSQL_PASSWORD';"
+        echo "FLUSH PRIVILEGES;"
+    } | mysqld --bootstrap
+else
+    echo "Mariadb is already configured"
+fi
 
 exec "$@"
